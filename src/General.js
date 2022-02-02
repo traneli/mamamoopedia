eee430d2b76f080083e2e290fc6ac97d3ceeddfd
etran95 <elluaurinko@hotmail.com>
2022-02-02 22:37:20 +0200

    New: Navigation bar on mobile works for now

---
+++ src/General.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div className="App">
+        {/* <video id='videoBackground' autoPlay loop muted>
+          <source src={sample} type='video/mp4'/>
+        </video> */}
+      </div>
+    );
+  }
+  
+  export default App;
