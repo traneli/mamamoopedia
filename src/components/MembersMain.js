522c9897c5b97f430cc0e5f9670fa97a616cdcdb
etran95 <elluaurinko@hotmail.com>
2022-02-09 23:08:32 +0200

    Update: Updated members photos

--- src/components/MembersMain.js
+++ src/components/MembersMain.js
@@ -17,22 +17,22 @@ function MembersMain() {
       <div class="members-main-content-image-group">
         <Link to={`${url}/moonbyul`} class="members-main-content-image-group-link">
           <button type="button" class="members-main-content-image-group-btn" onClick={() => setMember("moonbyul")}>
-            <img src={images["waw_moonbyul.jpg"].default}/>
+            <img src={images["thebest_moonbyul.jpg"].default}/>
           </button>
         </Link>
         <Link to={`${url}/solar`} class="members-main-content-image-group-link" onClick={() => setMember("solar")}>
           <button type="button" class="members-main-content-image-group-btn">
-            <img src={images["waw_solar.jpg"].default}/>
+            <img src={images["thebest_solar.jpg"].default}/>
           </button>
         </Link>
         <Link to={`${url}/wheein`} class="members-main-content-image-group-link" onClick={() => setMember("wheein")}>
           <button type="button" class="members-main-content-image-group-btn">
-            <img src={images["waw_wheein.jpg"].default}/>
+            <img src={images["thebest_wheein.jpg"].default}/>
           </button>
         </Link>
         <Link to={`${url}/hwasa`} class="members-main-content-image-group-link" onClick={() => setMember("hwasa")}>
           <button type="button" class="members-main-content-image-group-btn">
-            <img src={images["waw_hwasa.jpg"].default}/>
+            <img src={images["thebest_hwasa.jpg"].default}/>
           </button>
         </Link>
       </div>
